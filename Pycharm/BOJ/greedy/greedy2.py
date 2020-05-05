030df7aa5ef97e631e13e98a5ff6d5587190deb0
hyeon21-p <tjguspark14@gmail.com>
2020-05-06 06:36:41 +0900

    greedy2_ing

--- Pycharm/BOJ/greedy/greedy2.py
+++ Pycharm/BOJ/greedy/greedy2.py
@@ -1,4 +1,11 @@
 N = int(input())  #회의의 수
+list1 = []
+list2 = []
 
 for i in range(0, N):
-    
+    a = input().split(" ")
+    list1.append(int(a[0]))
+    list2.append(int(a[1]))
+
+I = (list1, list2)
+print(I)
